16023cc0cd851224e7be5c0ebe280a8106a6a60c
Jathmel Gordon <jathmel@gmail.com>
2018-05-18 22:45:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,11 +17,11 @@ class Song
   end
   
   def artist_name
-    # if artist.nil? 
-    #   nil 
-    # else
+    if artist.nil? 
+      nil 
+    else
       self.artist.name
-    # end
+    end
   end
   
 end
